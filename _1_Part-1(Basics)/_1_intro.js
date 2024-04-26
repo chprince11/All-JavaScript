6c803800bb7b8bef1593d71a92a0ab6f5f1904f8
chprince11 <chprince@sheridancollege.ca>
2024-04-25 22:33:48 -0400

    Added-part-1

---
+++ _1_Part-1(Basics)/_1_intro.js
@@ -0,0 +1,30 @@
+/* JavaScript
+
+- JS is a High-level, object-oriented, multi-paradigm programming language.
+
+- High-Level - We don't have to worry about complex stuff like memory management
+
+- Object-oriented - Based on objects for storing most kinds of data 
+
+- Multi-paradigm - It is so flexible and versatile, that we can use all kinds of different styles of programming, such as imperative and declarative programming.  
+
+- Programming lan - It means that it allows to write code that will instruct a computer to do things.
+
+** The role of JS in Web Developer **
+- HTML ( Content of the web page ) Nouns, 
+CSS ( presentaiton ) Adjectives, 
+JS ( allow to add dynamic and interactive effects to webpage ) Verbs
+
+- To open developer tools in browsers ( ctrl + shift + j ) or right click > inspect or simply hit f12
+
+** Also use JS outside the browser **
+
+Native mobile applications: ionic and react native frameworks
+Native Desktop applications: Electron framework
+
+** History **
+- ES5 ( 2014 ) > ES6 ( 2015 ) ( Major updates ) > ES7 > .. > ES11 > ..
+- ECMAScript ( ES )
+- ES6 - ES11 .. ( Known as Modern JS )
+
+*/

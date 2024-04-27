e6be71587dcbebde683b46c716076c5e32a17ac2
chprince11 <chprince@sheridancollege.ca>
2024-04-27 11:09:16 -0400

    JS-part-2

---
+++ Part-2/_1_intro.js
@@ -0,0 +1,19 @@
+/*
+ *** History of JS ***
+
+- Brendan Eich creates the very 1st verison of JS in just 10 days. It was called Mocha ( 1995 )
+
+- Mocha < LiveScript < JavaScript ( 1996 )
+
+- ECMA releases ECMAScript 1 ( ES1 ) ( 1997 )
+
+- ES5 - With lots of great new features. ( 2009 )
+
+- ES6 ( ES2015 ) - Biggest update to the language ever!
+- ECMAScript changes to an annual release cycle
+
+** Backwards Compatibility: Dont't break the web **
+** It is not Forward Compatibility **
+// During development: Simply use the latest Google Chrome!
+// During production: use Babel to transpile and polyfill your code
+ */

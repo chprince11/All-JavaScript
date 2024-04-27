d670b5673226e7ea8dcf10d9753092e509b69929
chprince11 <chprince@sheridancollege.ca>
2024-04-27 09:28:55 -0400

    JS-switch-case

---
+++ Part-1(Basics)/_11_switch.js
@@ -0,0 +1,28 @@
+const day = 'sunday';
+
+switch (day) {
+    case 'saturday':
+        console.log("I do a lot coding!");
+        break;
+    case 'sunday':
+        console.log("I watch movies and fun with friends!");
+        break;
+    case 'monday':
+    case 'tuesday':
+        console.log("Work on both monday and tuesday");
+        break;
+    default:
+        console.log("Not a valid day!");
+
+};
+
+/*
+ * Statements and Expressions:
+
+ - Expression is a piece of code that produces a value. 
+  ex: 10 + 9, true || false, 
+
+- Statements is like a bigger piece of code that is executed and does not produce a value on itself. 
+ ex: if( 23> 10) ( if-else), switch case, 
+ */
+

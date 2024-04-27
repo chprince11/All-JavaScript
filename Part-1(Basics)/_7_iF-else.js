54e79a6721fb02c80347c62a4dad219f1466ffb0
chprince11 <chprince@sheridancollege.ca>
2024-04-26 20:06:34 -0400

    JS-if-else

---
+++ Part-1(Basics)/_7_iF-else.js
@@ -0,0 +1,11 @@
+// *** Decisons making with if-else
+
+const age = 19;
+
+if ( age === 80) {
+    console.log(`No way to drive`);
+} else if (age > 18) {
+    console.log(`Can Drive!`);
+} else {
+    console.log(`Can't Drive!`);
+}

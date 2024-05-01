75a2aac8a282ef98b3c9ffd2ffa908693a26a664
chprince11 <chprince@sheridancollege.ca>
2024-04-30 21:31:56 -0400

    JS-DOM

--- 03-DOM-event-Fundamentals/_1_basics.js
+++ 03-DOM-event-Fundamentals/_1_basics.js
@@ -1,1 +1,24 @@
 'use strict'; // strict mode
+
+/*
+
+DOM ( Document Object Model )
+- Structured representation of HTML documnets.
+- Allows JS to access HTML elements and styles to manipulate them.
+-DOM is automatically created by the browser as soon as the HTML page loads and it's stored in a tree structure.
+-In this tree, each HTML element is one object 
+
+*/
+
+//Method that avaliable on Document object
+
+console.log(document.querySelector('.message').textContent);
+
+console.log(
+  (document.querySelector('.message').textContent = 'Correct number')
+);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);

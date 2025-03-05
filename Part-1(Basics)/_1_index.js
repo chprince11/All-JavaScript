ccac4b2424fee939bf12d16c77533aac7e35d456
chprince11 <princecan1102@gmail.com>
2025-03-04 22:09:51 -0500

    Added the JS

--- Part-1(Basics)/_1_index.js
+++ Part-1(Basics)/_1_index.js
@@ -1,6 +1,1 @@
 alert("This is External JS! putting the src path");
-
-/* JavaScript: JS is a high-level (dont't worry about the complex stuff like memory management), object-oriented, multi-paradigm (flexiblr and versatile, can use both imperative & declarative programming) programming language.
-- Can also run outside the web browers, like on web server with the technology Node.js (backend applications).
-- Huge update in 2015 so called ES2015 / ES6
-*/
